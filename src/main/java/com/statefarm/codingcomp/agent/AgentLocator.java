be1a97854d71d1314a63a584fe3198dfb4cbb6ab
Joshua Koh <Joshua@kohfamily.com>
2016-10-15 23:43:33 -0400

    Not working

--- src/main/java/com/statefarm/codingcomp/agent/AgentLocator.java
+++ src/main/java/com/statefarm/codingcomp/agent/AgentLocator.java
@@ -1,6 +1,7 @@
 package com.statefarm.codingcomp.agent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.statefarm.codingcomp.bean.Agent;
+import com.statefarm.codingcomp.bean.Office;
 import com.statefarm.codingcomp.bean.USState;
 import com.statefarm.codingcomp.utilities.SFFileReader;
 
@@ -22,7 +24,30 @@ public class AgentLocator {
 
 	@Autowired
 	private SFFileReader sfFileReader;
-
+	
+	private List<String> htmlFiles = sfFileReader.findHtmlFiles();
+	private List<String> agentFiles = sfFileReader.findAgentFiles();
+	
+	private Map<String, Agent> agentsByHTML = getAgentsByHTML();
+	private Map<String, Agent> agentsByAgentFiles = getAgentsByAgentFiles();
+	
+	private Map<String, Agent> getAgentsByHTML() {
+		Map<String, Agent> agents = new HashMap<String, Agent>();
+		for (String html : htmlFiles) {
+			agents.put(html, agentParser.parseAgent(html));
+		}
+		return agents;
+	}
+	
+	private Map<String, Agent> getAgentsByAgentFiles() {
+		Map<String, Agent> agents = new HashMap<String, Agent>();
+		for (String html : agentFiles) {
+			agents.put(html, agentParser.parseAgent(html));
+		}
+		return agents;
+	}
+	
+	
 	/**
 	 * Find agents where the URL of their name contains the firstName and
 	 * lastName For instance, Tom Newman would search for "Tom-" and "-Newman"	
@@ -33,15 +58,11 @@ public class AgentLocator {
 	 * @return
 	 */
 	public List<Agent> getAgentsByName(String firstName, String lastName) {
-		System.out.println("hey");
-		List<String> myList = sfFileReader.findHtmlFiles();
 		List<Agent> myAgents = new ArrayList<Agent>();
-		for(String str: myList) {
-		    if((str.trim().contains(firstName + "-")) && (str.trim().contains("-" + lastName))){
-		    	System.out.println(str);
-		    	myAgents.add(agentParser.parseAgent(str));
+		for (String html : htmlFiles) {
+		    if ((html.trim().contains(firstName + "-")) && (html.trim().contains("-" + lastName))){
+		    	myAgents.add(agentsByHTML.get(html));
 		    }
-		       //return str;
 		}
 		return myAgents;
 	}
@@ -50,16 +71,28 @@ public class AgentLocator {
 	 * Find agents by state.
 	 * 
 	 * @param state
-	 * @return
+	 * @return a list of agents who operate in the state given by state 
 	 */
 	public List<Agent> getAgentsByState(USState state) {
-		return null;
+		List<String> agentHtmlFiles = sfFileReader.findAgentFiles();
+		List<Agent> allAgents = getAllAgents();
+        List<Agent> agentsByState = new ArrayList<Agent>();
+        
+        for(Agent a: allAgents) {
+            for (Office o : a.getOffices()) {
+        		if (o.getAddress().getState() == state) {
+        			agentsByState.add(a);
+        			continue;
+        		}
+        	}
+        }
+        return agentsByState;
 	}
 
 	public List<Agent> getAllAgents() {
 		return null;
 	}
-
+	
 	public Map<String, List<Agent>> getAllAgentsByUniqueFullName() {
 		return null;
 	}
