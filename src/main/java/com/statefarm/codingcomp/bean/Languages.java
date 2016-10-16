7f93178b9482b6beaa3ded62f24b7e193f3f4895
Joshua Koh <Joshua@kohfamily.com>
2016-10-16 00:53:21 -0400

    languages javadoc

--- src/main/java/com/statefarm/codingcomp/bean/Languages.java
+++ src/main/java/com/statefarm/codingcomp/bean/Languages.java
@@ -3,16 +3,27 @@ package com.statefarm.codingcomp.bean;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Languages singleton
+ *
+ */
 public class Languages {
 
 	private static Languages instance = null;
 	private static Map<String, String> langMap;
 	
+	/**
+	 * Prevent instantiation
+	 */
 	protected Languages() {
 		langMap = new HashMap<String, String>();
 		buildLangMap();
 	}
 	
+	
+	/**
+	 * Allow an instance to be created or referenced
+	 */
 	public static Languages getInstance() {
 		if (instance == null) {
 			instance = new Languages();
@@ -20,12 +31,20 @@ public class Languages {
 		return instance;
 	}
 	
+	/**
+	 * Build map based on languages which appear on agent pages
+	 */
 	private static void buildLangMap() {
 		langMap.put("English", "English");
 		langMap.put("Español", "Spanish");
 		langMap.put("Deutsch", "German");
 	}
 	
+	/**
+	 * Convert from native language name to English language name
+	 * @param key native langauge name
+	 * @return English language name
+	 */
 	public static String getEnglishLang(String key) {
 		return langMap.get(key);
 	}
