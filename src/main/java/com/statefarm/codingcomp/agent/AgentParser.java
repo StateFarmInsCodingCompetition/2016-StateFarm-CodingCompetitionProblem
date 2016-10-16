50c7b4335eb93fcde388d86a46d4f8ba87b6191c
Antonio Rios <antonio.rios.jr@gmail.com>
2016-10-15 21:55:06 -0700

    Made a further progress, but still can't pass the first JUnit test

--- src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
+++ src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
@@ -17,6 +17,7 @@ import com.statefarm.codingcomp.bean.Address;
 import com.statefarm.codingcomp.bean.Agent;
 import com.statefarm.codingcomp.bean.Office;
 import com.statefarm.codingcomp.bean.Product;
+import com.statefarm.codingcomp.bean.USState;
 import com.statefarm.codingcomp.utilities.SFFileReader;
 
 @Component
@@ -47,7 +48,6 @@ public class AgentParser {
 		
 		// now I need to parse the products
 		Element productslist = html.body().getElementById("sfx_defaultToggle_div");
-		Elements agentproducts = productslist.getElementsByAttributeValue("itemprop", "description");
 		Elements products = productslist.getElementsByTag("li");
 		
 		Set<Product> setproducts = new HashSet<Product>();
@@ -71,23 +71,81 @@ public class AgentParser {
 		Address address2 = new Address();
 		
 		Element parseELang = html.body().getElementById("languageEnglish_mainLocContent");
-		System.out.println(parseELang.text());
-		languages1.add(parseELang.text());
 		
 		Element parseSLang = html.body().getElementById("languageSpanish_mainLocContent_0");
-		System.out.println(parseSLang.text());
-		languages2.add(parseSLang.text());
 		
-		Element parseOfficeHours = html.body().getElementById("officeHoursContent_mainLocContent_0");
-		Element parseAddress1 = html.body().getElementById("mailAddressContent_mainLocContent");
-		Element parseAddress2 = html.body().getElementById("mailAddressAdContent_mainLocContent");
+		Element parseOffice1Hours1 = html.body().getElementById("officeHoursContent_mainLocContent_0");
+		Element parseOffice1Hours2 = html.body().getElementById("officeHoursContent_mainLocContent_1");
+		Element parseOffice1Hours3 = html.body().getElementById("officeHoursContent_mainLocContent_2");
+
+		Element parseAddress1Line1 = html.body().getElementById("mailAddressContent_mainLocContent");
+		Element parseAddress1Line2 = html.body().getElementById("mailAddressAdContent_mainLocContent");
+		Elements spans = parseAddress1Line2.getElementsByTag("span");
 		
-		System.out.println(parseOfficeHours.text());
-		System.out.println(parseAddress1.text());
-		address1.setLine1(parseAddress1.text());
+		Elements officephone = html.body().getElementById("offNumber_mainLocContent").getElementsByTag("span");
+		String phone = officephone.get(1).text();
 		
-		// I lost a lot of time trying to understand how to use Jsoup and parse the html file.
+		List<String> address = new ArrayList<String>(); 
+		for(Element m : spans){
+			address.add(m.text());
+		}
 		
-		return null;
+		languages1.add(parseELang.text());
+		languages1.add(parseSLang.text());
+		office1Hours.add(parseOffice1Hours1.text());
+		office1Hours.add(parseOffice1Hours1.text());
+		office1Hours.add(parseOffice1Hours1.text());
+		address1.setLine1(parseAddress1Line1.text());
+		address1.setLine2(null);
+		address1.setCity(address.get(1));
+		address1.setState(USState.fromValue(address.get(2)));
+		address1.setPostalCode(address.get(3));
+		office1.setLanguages(languages1);
+		office1.setPhoneNumber(phone);
+		office1.setOfficeHours(office1Hours);
+		office1.setAddress(address1);
+		
+
+		Element secondOfficeELang = html.body().getElementById("languageEnglish_additionalLocContent_0");
+		Element secondOfficeSLang = html.body().getElementById("languageSpanish_additionalLocContent_0_0");
+		
+		languages2.add(secondOfficeELang.text());
+		languages2.add(secondOfficeSLang.text());
+		
+		Element secondOfficeHoursLine1 = html.body().getElementById("officeHoursContent_additionalLocContent_0_0");
+		Element secondOfficeHoursLine2 = html.body().getElementById("officeHoursContent_additionalLocContent_0_1");
+		Element secondOfficeHoursLine3 = html.body().getElementById("officeHoursContent_additionalLocContent_0_2");
+		
+		office2Hours.add(secondOfficeHoursLine1.text());
+		office2Hours.add(secondOfficeHoursLine2.text());
+		office2Hours.add(secondOfficeHoursLine3.text());
+		
+		Element secondoffice = html.body().getElementById("paneladditionalLoc_0");
+		Elements firstline = secondoffice.select("span#locStreetContent_additionalLocContent_0");
+		Elements secondlocal = secondoffice.getElementsByAttributeValue("itemprop", "addressLocality");
+		Elements region = secondoffice.getElementsByAttributeValue("itemprop", "addressRegion");
+		Elements postalcode = secondoffice.getElementsByAttributeValue("itemprop", "postalCode");
+		
+		String addressfirstline = firstline.text().substring(0,25);
+		String addresssecondline = firstline.text().substring(25);
+		
+		address2.setLine1(addressfirstline);
+		address2.setLine2(addresssecondline);
+		address2.setCity(secondlocal.text());
+		address2.setState(USState.fromValue(region.text()));
+		address2.setPostalCode(postalcode.text());
+		
+		office2.setLanguages(languages2);
+		office2.setPhoneNumber(phone);
+		office2.setOfficeHours(office2Hours);
+		office2.setAddress(address2);		
+		
+		List<Office> offices = new ArrayList<Office>();
+		offices.add(office1);
+		offices.add(office2);
+		
+		actualAgent.setOffices(offices);
+		
+		return actualAgent;
 	}
 }
