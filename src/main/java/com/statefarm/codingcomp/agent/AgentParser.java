8c9939ee4602989d0ec2f9aa66b1d99a3a2f0615
Joshua Koh <Joshua@kohfamily.com>
2016-10-15 21:25:39 -0400

    first push

--- src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
+++ src/main/java/com/statefarm/codingcomp/agent/AgentParser.java
@@ -1,5 +1,12 @@
 package com.statefarm.codingcomp.agent;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -14,6 +21,15 @@ public class AgentParser {
 
 	@Cacheable(value = "agents")
 	public Agent parseAgent(String fileName) {
+		Agent agent = new Agent();
+		File input = new File(fileName);
+		try {
+			Document doc = Jsoup.parse(input, "UTF-8", "");
+			System.out.println(doc.body().text());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 }
